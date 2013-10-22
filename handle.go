1aa562770dd8856255835b3b051f228d1f045373
Reed O'Brien <reed@reedobrien.com>
2013-10-22 14:47:27 -0400

    move from ServeMux.HandleFunc to http.Handler
    This way we can wrap it in a timeout handler and
    return timeouts if it is taking too long.

--- handle.go
+++ handle.go
@@ -5,31 +5,55 @@
 package rescat
 
 import (
+	"errors"
 	"fmt"
+	"log"
 	"net/http"
-	"net/url"
 	"strings"
 )
 
 var VERSION string = "2013.10.16"
 var MARK string = "??"
 
-// A basic handler.
-func HandleFS(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Server", fmt.Sprintf("rescat/%s", VERSION))
-	p := &Provide{}
-	p.Fetcher = &FetchFile{}
-	parseUrl(r.URL, p, w, r)
-	fmt.Fprintf(w, "%v", r)
+// TODO: This should 'implement' provider and fetcher
+// TODO: so we don't have to pass the w and r around
+type HandleFS struct {
+	p *Provide
 }
 
-func parseUrl(u *url.URL, p *Provide, w http.ResponseWriter, r *http.Request) (err error) {
-	v := strings.Split(u.String(), MARK)
+// A basic handler.
+func (h *HandleFS) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Server", fmt.Sprintf("rescat/%s", VERSION))
+	p := &Provide{Fetcher: &FetchFile{}}
+	// p.Fetcher = &FetchFile{}
+	err := h.parseUrl(w, r, p)
+	if err != nil {
+		log.Println("error rescat.HandleFS.ServeHTTP: ", err)
+		return
+	}
+	x, err := p.Provide()
+	// TODO: refactor with HandleFS as Provider and Fetcher
+	if err != nil {
+		if err == errors.New("not found") {
+			http.NotFound(w, r)
+		} else {
+			http.Error(w, "", http.StatusInternalServerError)
+		}
+	}
+	fmt.Fprintf(w, "%s", x)
+}
+
+func (h *HandleFS) parseUrl(w http.ResponseWriter, r *http.Request, p *Provide) error {
+	v := strings.Split(r.URL.RequestURI(), MARK)
 	if len(v) != 2 {
 		badRequest(w, r)
-		return err
+		return errors.New("bad request")
+	} else {
+		// strip the leading and training / we'll join on them else
+		p.Path = strings.Trim(v[0], "/")
+		p.Names = strings.Split(v[1], ",")
 	}
-	return err
+	return nil
 }
 
 func badRequest(w http.ResponseWriter, r *http.Request) {
